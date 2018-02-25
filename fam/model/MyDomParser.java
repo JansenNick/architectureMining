2f561d94c4cabe9a1edbee48ca030552064b8e76
NJ <nickjansen9@hotmail.com>
2018-02-25 21:16:17 +0100

    Added infoFlow to domParser

--- fam/model/MyDomParser.java
+++ fam/model/MyDomParser.java
@@ -18,10 +18,12 @@ public class MyDomParser {
 	
 	public static void main(String[] args) {
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		
 		try {
 			DocumentBuilder builder = factory.newDocumentBuilder();
 			Document doc = builder.parse("src\\org\\architecturemining\\fam\\model\\FAM.xml");
 			NodeList nodeList = doc.getElementsByTagName("node");
+			NodeList infoFlowList = doc.getElementsByTagName("informationFlow");
 			
 			//looping through nodeList
 			for(int i=0; i < nodeList.getLength() ; i++) {
@@ -31,9 +33,9 @@ public class MyDomParser {
 					short type = node.getNodeType();
 					NodeList nameList = node.getChildNodes();
 					System.out.println("------------------");
-					result = result + "------------------";
+
 					System.out.println(node.getAttribute("type"));
-					result = result + node.getAttribute("type");
+
 					
 					//looping through nameList
 					for(int j=0;j<nameList.getLength();j++) {
@@ -44,7 +46,34 @@ public class MyDomParser {
 								//System.out.println(name.getTagName());
 							//}
 							System.out.println(name.getTagName() + ":" + name.getTextContent());
-							result = result + name.getTagName() + ":" + name.getTextContent();
+
+							//type, name.getTagName(), name.getTextContent()
+						}
+					}
+				}
+			}
+			
+			
+			//looping through infoFlowList
+			for(int i=0; i < infoFlowList.getLength() ; i++) {
+				Node p = infoFlowList.item(i);
+				if(p.getNodeType() == Node.ELEMENT_NODE) {
+					Element infoFlow = (Element) p;
+					//short type = infoFlow.getNodeType();
+					NodeList nameList = infoFlow.getChildNodes();
+					System.out.println("------------------");
+					System.out.println(infoFlow.getAttribute("type"));
+					
+					//looping through nameList
+					for(int j=0;j<nameList.getLength();j++) {
+						Node n = nameList.item(j);
+						if(n.getNodeType() == infoFlow.ELEMENT_NODE) {
+							Element nameInfoFlow = (Element) n;
+							//if(name.getTagName() == "name") {
+								//System.out.println(name.getTagName());
+							//}
+							System.out.println(nameInfoFlow.getTagName() + ": " + nameInfoFlow.getTextContent());
+							System.out.println("" + infoFlow.getAttributes().getNamedItem("source") + infoFlow.getAttributes().getNamedItem("target"));
 							//type, name.getTagName(), name.getTextContent()
 						}
 					}
