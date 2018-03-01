900c9bfc3038a1705acce7b14a0615d0eae5a365
Nick Jansen <nickjansen9@hotmail.com>
2018-03-01 13:56:54 +0100

    Finally got writing and reading of JDOM parser to work

--- fam/model/JDOMparser.java
+++ fam/model/JDOMparser.java
@@ -2,48 +2,103 @@ package org.architecturemining.fam.model;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 import org.jdom2.Document;
 import org.jdom2.Element;
+import org.jdom2.JDOMException;
 import org.jdom2.Text;
+import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
 
 public class JDOMparser {
 	
 	public static void main(String[] args) {
-		
 	
 	writeXML();
 	
-	//readXML();
+	readXML();
+	
 	}
 
+private static void readXML() {
+	
+	SAXBuilder builder = new SAXBuilder();
+		
+		Document readDoc = null;
+		
+		try {
+			readDoc = builder.build(new File("./src/jdomMade.xml"));
+		} catch (JDOMException | IOException e) {
+
+			e.printStackTrace();
+		}
+		
+		Element root = readDoc.getRootElement();
+		
+		System.out.println("---------------------");
+		
+		for (Element moduleEle : root.getChildren("FAMnode")) {
+			
+			System.out.println(moduleEle.getAttributeValue("type") + " " + moduleEle.getChildText("name"));
+			System.out.println("Origin: " + moduleEle.getChildText("origin"));
+			System.out.println("Height: " + moduleEle.getChildText("heigth"));
+			System.out.println("Width: " + moduleEle.getChildText("width"));
+			System.out.println("Has features:");
+			
+			for (Element featureEle : moduleEle.getChildren("FAMnode")) {
+				
+				System.out.println("\t" + featureEle.getAttributeValue("type") + " " + featureEle.getChildText("name"));
+				System.out.println("\t" + "Origin: " + featureEle.getChildText("origin"));
+				System.out.println("\t" + "Height: " + featureEle.getChildText("heigth"));
+				System.out.println("\t" + "Width: " + featureEle.getChildText("width"));				
+			}
+			
+			System.out.println("---------------------");
+		}
+		
+		for (Element lineEle : root.getChildren("line")) {
+			
+			System.out.println(lineEle.getAttributeValue("type") + " " + lineEle.getChildText("name"));	
+			System.out.println("Source: " + lineEle.getAttributeValue("source"));
+			System.out.println("Target: " + lineEle.getAttributeValue("target"));
+			System.out.println("---------------------");
+		}
+	
+}
 
 private static void writeXML() {
 	try {
 	Document doc = new Document();
-	
-	Element theRoot = new Element("tvshows");
+	//create root
+	Element theRoot = new Element("FAM");
 	doc.setRootElement(theRoot);
 	
-	Element show = new Element("show");
+	//Mogelijk modules/features/lines niet eerst in Elements op te slaan door parent mee te geven aan de functie
+	//Gaat alleen mis bij het creeren van een feature omdat je dan de naam van een child van theRoot moet hebben
+	//Echter geef ik dergelijke childs nergens een naam
+	//creation of modules
+	Element moduleA = createFAMnode("module", "A", "130,45", "60", "120");
+	Element moduleB = createFAMnode("module", "B", "491,45", "208", "365");
 	
-	Element name = new Element("name");
-	name.setAttribute("show_id", "show_001");
-	name.addContent(new Text("Life on Mars"));
+	//creation of features
+	Element featureQ = createFAMnode("feature", "Q", "140,71", "60", "120");
+	Element featureR = createFAMnode("feature", "R", "520,144", "60", "120");
+	Element featureP = createFAMnode("feature", "P", "726,71", "60", "120");
 	
-	Element network = new Element("network");
-	network.setAttribute("country", "US");
-	network.addContent(new Text("ABC"));
+	//creation of lines
+	Element line1 = createLine("infoFlow","infoFlow1", "V", "S");
 	
-	show.addContent(name);
-	show.addContent(network);
+	//adding features to modules
+	moduleA.addContent(featureQ);
+	moduleB.addContent(featureR);
+	moduleB.addContent(featureP);
 	
-	theRoot.addContent(show);
-	
-	//  -----------
-	
+	//adding modules and lines to root
+	theRoot.addContent(moduleA);
+	theRoot.addContent(moduleB);
+	theRoot.addContent(line1);
 	
 	XMLOutputter xmlOutput = new XMLOutputter(Format.getPrettyFormat());
 	
@@ -56,4 +111,53 @@ private static void writeXML() {
 		ex.printStackTrace();
 	}
 }
+
+private static Element createFAMnode(String nodeType, String nodeName, String nodeOrigin, String nodeHeight, String nodeWidth) {
+	
+	Element FAMnode = new Element("FAMnode");
+	FAMnode.setAttribute("type", nodeType);
+		
+		//create first node in FAMnode
+		Element name = new Element("name");
+		//add content to first node
+		name.addContent(new Text(nodeName));
+	
+		//create second node in show node
+		Element origin = new Element("origin");
+		//add content to second node
+		origin.addContent(new Text(nodeOrigin));
+		
+		//create second node in show node
+		Element height = new Element("height");
+		//add content to second node
+		height.addContent(new Text(nodeHeight));
+		
+		//create second node in show node
+		Element width = new Element("width");
+		//add content to second node
+		width.addContent(new Text(nodeWidth));
+	
+	FAMnode.addContent(name);
+	FAMnode.addContent(origin);
+	FAMnode.addContent(height);
+	FAMnode.addContent(width);
+	
+	return FAMnode;
+}
+
+private static Element createLine(String lineType, String lineName, String source, String target) {
+	Element line = new Element("line");
+	
+	line.setAttribute("type", lineType);
+	line.setAttribute("source", source);
+	line.setAttribute("target", target);
+	
+		Element name = new Element("name");
+		name.addContent(new Text(lineName));
+	
+	line.addContent(name);
+	
+	return line;
+}
+
 }
